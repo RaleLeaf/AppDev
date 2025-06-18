fd082e9fae32c16c5c8df2aa7ed7b01133af0148
RaleLeaf <168341542+RaleLeaf@users.noreply.github.com>
2025-06-18 20:51:20 +0800

    Splash screen

--- baSICK/src/main.jsx
+++ baSICK/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Test from './components/test.jsx';
+import Splash from './components/Splash.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Splash />
   </StrictMode>,
 )
