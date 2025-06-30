1b7411283957c65da0a9627e65d9eb9860808a24
James Garcia <11700004@usc.edu.ph>
2025-06-30 20:54:29 +0800

    Update FitnessLeaderboard.jsx

--- client/src/components/FitnessLeaderboard.jsx
+++ client/src/components/FitnessLeaderboard.jsx
@@ -149,7 +149,7 @@ const FitnessLeaderboard = () => {
         </div>
 
         {/* Leaderboard List */}
-        <div className="px-7 flex-1 mt-2">
+        <div className="px-10 md:px-32 flex-1 mt-2">
           {leaderboardData.map((user) => (
             <div key={user.id} className="relative mb-4">
               <div className="absolute -left-2 top-1/2 transform -translate-y-1/2 -translate-x-1/2 w-9 h-9 rounded-full bg-black border-2 border-lime-500 flex items-center justify-center text-white font-bold">
